44397ea5a4f3d24eaf8c39f571403c5fdf735158
lachlan <lachlan.miller.1990@outlook.com>
2017-11-22 17:51:21 +0900

    first

---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello'
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/setData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'setData'
+  }
+</script>
+
+<style scoped>
+</style>
